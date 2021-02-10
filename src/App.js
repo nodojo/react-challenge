b42632cdc8464083e3475e6ccbcfc7e983182459
noDojo <nullDojo@gmail.com>
2021-02-10 07:01:34 -0500

    Connect to api and pull data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let getUsers = () => axios.get('https://jsonplaceholder.typicode.com/users');
+let getPosts = () => axios.get('https://jsonplaceholder.typicode.com/posts');
+
+class App extends Component {
+  state = {
+    users: [],
+    posts: [],
+    headers: ['USERS', 'POSTS'],
+  };
+
+  componentDidMount() {
+    const obj = this;
+
+    axios
+      .all([getUsers(), getPosts()])
+      .then(
+        axios.spread((user, post) => {
+          const users = user.data;
+          const posts = post.data;
+          obj.setState({ users, posts });
+        })
+      )
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    const obj = this;
+
+    return (
+      <table className='ui fixed table'>
+        <thead>
+          <tr>
+            <th>{obj.state.headers[0]}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {obj.state.users.map((user) => (
+            <tr>
+              <td>{user.name}</td>
+            </tr>
+          ))}
+        </tbody>
+        <thead>
+          <tr>
+            <th>{obj.state.headers[1]}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {obj.state.posts.map((post) => (
+            <tr>
+              <td>{post.title}</td>
+              <td>{post.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default App;
